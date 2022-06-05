5fe044420473930678d7c1317e0eb1f5b8ca7c1a
Antoine Lolliot <antoinelolliot@gmail.com>
2022-06-04 23:02:21 -0500

    css changes to content boxes and transitions

--- modules/zlta-interactive-map/includes/frontend.php
+++ modules/zlta-interactive-map/includes/frontend.php
@@ -25,12 +25,14 @@
         <?php foreach ( $settings->modal_content_form as $modal) : ?>
             <div class="modal-box" id="modal_Run<?php echo $run_index; ?>">
                 <h3 class="runHeader"><?php echo $modal->run_title; ?></h3>
-                <img src="<?php echo $modal->run_photo_src; ?>" />
+                <div class = "ul-image--wrapper">
+                <img class = "modal-image" src="<?php echo $modal->run_photo_src; ?>" />
                 <ul id = "descriptorList" style="list-style-type:none;">
                     <li class="speed">Max Speed: <?php echo $modal->run_max_speed; ?> mph</li>
                     <li class="distance">Distance: <?php echo $modal->run_distance; ?>' </li>
                     <li class="elevation">Elevation: <?php echo $modal->run_elevation; ?>'</li>
                 </ul>
+                </div>
             </div>
         <?php $run_index++; ?>
         <?php endforeach; ?>
