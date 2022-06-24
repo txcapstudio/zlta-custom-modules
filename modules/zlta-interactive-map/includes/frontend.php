5b083ce96927894197315bfdc7f0a0219f067114
Dylan Wagner <dylan@txcapstudio.com>
2022-06-24 16:01:44 -0500

    final map updates

--- modules/zlta-interactive-map/includes/frontend.php
+++ modules/zlta-interactive-map/includes/frontend.php
@@ -7,37 +7,39 @@
     <div class="zlta-interactive-map--image-container">
         <div class="zlta-interactive-map--svg">
             <?php
-            $mapPath = esc_url(site_url() . "/wp-content/plugins/zlta-custom-modules/modules/zlta-interactive-map/assets/RunsOnly.svg");
+            $mapPath = esc_url(site_url() . "/wp-content/plugins/zlta-custom-modules/modules/zlta-interactive-map/assets/ZLTA_Map_Runs_v3.svg");
             echo file_get_contents($mapPath);
             ?>
         </div>
         <div class="zlta-interactive-map--base">
-            <img src="<?php echo esc_url(site_url(). '/wp-content/plugins/zlta-custom-modules/modules/zlta-interactive-map/assets/ZLTABase.jpg'); ?>" />
+            <img src="<?php echo esc_url(site_url(). '/wp-content/plugins/zlta-custom-modules/modules/zlta-interactive-map/assets/ZLTA_Map_Base_v2.png'); ?>" />
         </div>
     </div>
 
 
     <div class="modal-box--wrapper">
 
-        <a href="#" class="close-btn">&times;</a>
+        <div class="modal-box--overlay"></div>
 
-        <?php $run_index = 3; ?>
-        <?php foreach ( $settings->modal_content_form as $modal) : ?>
-            <div class="modal-box" id="modal_Run<?php echo $run_index; ?>">
-                <a href="#" class="close-btn">&times;</a>
-                <h3 class="runHeader"><?php echo $modal->run_title; ?></h3>
+        <div class="modal-box--container">
+            <a href="#" class="close-btn">&times;</a>
 
-                <div class = "ul-image--wrapper">
-                <img class = "modal-image" src="<?php echo $modal->run_photo_src; ?>" />
-                <ul id = "descriptorList" style="list-style-type:none;">
-                    <li class="speed">Max Speed: <?php echo $modal->run_max_speed; ?> mph</li>
-                    <li class="distance">Distance: <?php echo $modal->run_distance; ?>' </li>
-                    <li class="elevation">Elevation: <?php echo $modal->run_elevation; ?>'</li>
-                </ul>
+            <?php $run_index = 3; ?>
+            <?php foreach ( $settings->modal_content_form as $modal) : ?>
+                <div class="modal-box" id="modal_Run<?php echo $run_index; ?>">
+                    <h3 class="runHeader"><?php echo $modal->run_title; ?></h3>
+                    <div class = "ul-image--wrapper">
+                    <img class = "modal-image" src="<?php echo $modal->run_photo_src; ?>" />
+                    <ul id = "descriptorList" style="list-style-type:none;">
+                        <li class="speed">Max Speed: <?php echo $modal->run_max_speed; ?> mph</li>
+                        <li class="distance">Distance: <?php echo $modal->run_distance; ?>' </li>
+                        <li class="elevation">Elevation: <?php echo $modal->run_elevation; ?>'</li>
+                    </ul>
+                    </div>
                 </div>
-            </div>
-        <?php $run_index++; ?>
-        <?php endforeach; ?>
+            <?php $run_index++; ?>
+            <?php endforeach; ?>
+        </div>
 
     </div>
 </div>
--- modules/zlta-interactive-map/js/frontend.js
+++ modules/zlta-interactive-map/js/frontend.js
@@ -1,49 +1,33 @@
 // SVG Paths - Click
 elements = Array.from(document.querySelectorAll('svg #Run3, #Run4, #Run5, #Run6, #Run7'));
-elements.forEach(function(el) {
+elements.forEach( function(el) {
     el.addEventListener("click", start);
 });
 
 // ZipLine Runs - Hover
 let ziplineRuns = Array.from(document.querySelectorAll('.svg_zipline_run'));
 ziplineRuns.forEach( (el) => {
-   el.addEventListener('mouseenter', () => {
-        console.log('reduce opacity on any not hovered');
-       let zipTooltipsPopup = document.getElementById(el.id + "_Tooltip")
-       zipTooltipsPopup.classList.add("hideToolTip");
+
+    // Run hover
+    el.addEventListener('mouseenter', () => {
         ziplineRuns.forEach( (newEl) => {
-            newEl.classList.add('svg_zipline_run--reduce-opacity');
-         let zipTooltipsPopup = document.getElementById(newEl.id + "_Tooltip")
-            zipTooltipsPopup.classList.add("hideToolTip");
+            if ( newEl !== el ) {
+                newEl.style.opacity = 0.5;
+            }
+            // newEl.classList.add('svg_zipline_run--reduce-opacity');
         } );
-        el.classList.remove('svg_zipline_run--reduce-opacity');
-        zipTooltipsPopup.classList.remove("hideToolTip");
+        // el.classList.remove('svg_zipline_run--reduce-opacity');
+    });
 
-
-        // For tooltip
-        // Get ID of el
-        // Show {elId}_Tooltip
-   });
-    let zipTooltipsPopup = document.getElementById(this.id)
-
-
-
-   el.addEventListener('mouseout', () => {
+    // Run mouseout
+    el.addEventListener('mouseleave', () => {
        ziplineRuns.forEach( (newEl) => {
-           newEl.classList.remove('svg_zipline_run--reduce-opacity');
-           let zipToolTipsRemover = document.getElementById(newEl.id + "_Tooltip");
-           zipToolTipsRemover.classList.remove("hideToolTip")
-       } );
-
-       // For tooltip
-       // Just hide all tooltips
-   });
+           newEl.style.opacity = 1.0;
+           // newEl.classList.remove('svg_zipline_run--reduce-opacity');
+       });
+    });
 
 });
-
-
-
-
 
 // Close Button
 document.querySelector('.modal-box--wrapper a.close-btn').addEventListener('click', (e) => {
@@ -53,8 +37,6 @@ document.querySelector('.modal-box--wrapper a.close-btn').addEventListener('clic
 });
 
 // event listener functions
-
-
 function start(e){
     let modal = document.querySelector(".modal-box--wrapper")
     let closeBtn = document.querySelector(".close-btn")
@@ -66,33 +48,12 @@ function start(e){
 
     targetModal.classList.add('modal-content');
 
-
     modal.style.display = "block";
 
     window.onclick = function(e){
-        if(e.target == modal){
+        if( e.target == modal ){
             modal.style.display = "none"
         }
     }
 
 }
-
-
-//  tool tip functionality
-// function showTooltip(evt, text) {
-//     let tooltip = document.getElementById("tooltip");
-//     tooltip.innerHTML = text;
-//     tooltip.style.display = "block";
-//     tooltip.style.left = evt.pageX + 10 + 'px';
-//     tooltip.style.top = evt.pageY + 10 + 'px';
-// }
-//
-// function hideTooltip() {
-//     var tooltip = document.getElementById("tooltip");
-//     tooltip.style.display = "none";
-// }
-
-
-
-
-// onmouseenter="showTooltip(evt, 'Run #7');" onmouseout="hideTooltip();"
